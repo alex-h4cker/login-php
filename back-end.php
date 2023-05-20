78eee3905453833e9d2236cd8436733503c676d1
Alex <130258074+alex-h4cker@users.noreply.github.com>
2023-05-19 20:22:20 -0400

    Delete back-end.php

--- back-end.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if($_SERVER['REOUEST_METHOD'] == 'POST'){
-    if ($_POST['email'] == 'ali@gmail.com' && $_POST['password'] == 'ali') {
-        //
-    } else {
-        header(location: login.php)
-    }
-}
-?>
